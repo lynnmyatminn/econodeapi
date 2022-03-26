eb4fab77861dfaf12ccb5de142708a8a00b48b37
lynnmyatminn <lynnmyatminn@gmail.com>
2022-03-26 20:20:36 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+require('dotenv/config');
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+app.use(cors());
+app.use('*', cors());
+
+const api = process.env.API_URL;
+
+const productsRouter = require('./routers/product');
+const categoriesRouter = require('./routers/category');
+const ordersRouter = require('./routers/order');
+const usersRouter = require('./routers/user');
+
+//middleware
+app.use(express.json());
+app.use(morgan('tiny'));
+
+//Routers
+app.use(`${api}/products`, productsRouter);
+app.use(`${api}/orders`, ordersRouter);
+app.use(`${api}/categories`, categoriesRouter);
+app.use(`${api}/users`, usersRouter);
+
+//db connect
+mongoose.connect(process.env.DB_CONNECTION_STRING)
+    .then(() => {
+        console.log('Database connection is ready...');
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+
+app.listen(3000, () => {
+    console.log(`server is running at port 3000`);
+});
---
+++ routers/user.js
@@ -0,0 +1,33 @@
+const { User } = require('../models/user');
+const express = require('express');
+const router = express.Router();
+
+router.get(`/`, async (req, res, next) => {
+
+    const userList = await User.find();
+
+    if (!userList) {
+        res.status(500).json({ success: false })
+    }
+
+    res.send(userList)
+});
+
+router.post(`/`, (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock
+    })
+
+    user.save().then((createdUser => {
+        res.status(201).json(createdUser)
+    })).catch((err) => {
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+    })
+});
+
+module.exports = router;
