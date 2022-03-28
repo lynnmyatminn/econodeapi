21bf4feab45c41c8eea2bd0c3aa41eaba079263f
lynnmyatminn <lynnmyatminn@gmail.com>
2022-03-28 22:56:25 +0700

    Added Order & OrderItems

--- models/order.js
+++ models/order.js
@@ -1,12 +1,79 @@
 const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-        type: Number,
+    orderItems: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'OrderItem',
         required: true
+    }],
+    shippingAddress1: {
+        type: String,
+        required: true
+    },
+    shippingAddress2: {
+        type: String
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    zip: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        default: 'Pending'
+    },
+    totalPrice: {
+        type: Number,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now,
     }
 });
 
+orderSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+orderSchema.set('toJSON', {
+    virtuals: true
+});
+
 exports.Order = mongoose.model('Order', orderSchema);
+
+/**
+ {
+     "orderItems": [
+         {
+             "quantity": 4,
+             "product": "623d3d1ad2c075efcf833b63"
+         },
+          {
+             "quantity": 8,
+             "product": "623d3d3bd2c075efcf833b65"
+         },
+     ],
+     "shippingAddress1": "bangkok",
+     "shippingAddress2": "thailand",
+     "city":"bangkok",
+     "zip":"10310",
+     "country":"thailand",
+     "phone": "123456789",
+     "user":"623f21cdd00a64c5302bf8c1"
+ }
+ */
--- routers/order.js
+++ routers/order.js
@@ -1,10 +1,18 @@
 const { Order } = require('../models/order');
 const express = require('express');
+const { OrderItem } = require('../models/order-item');
 const router = express.Router();
 
 router.get(`/`, async (req, res, next) => {
 
-    const orderList = await Order.find();
+    const orderList = await Order.find()
+        .populate('user', 'name')
+        .populate({
+            path: 'orderItems', populate: {
+                path: 'product', populate: 'category'
+            }
+        })
+        .sort('dateCreated');
 
     if (!orderList) {
         res.status(500).json({ success: false })
@@ -13,21 +21,73 @@ router.get(`/`, async (req, res, next) => {
     res.send(orderList)
 });
 
-router.post(`/`, (req, res) => {
-    const order = new Order({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
+router.get(`/:id`, async (req, res, next) => {
+
+    const order = await Order.findById(req.params.id)
+        .populate('user', 'name')
+        .populate({
+            path: 'orderItems', populate: {
+                path: 'product', populate: 'category'
+            }
+        })
+        .sort('dateCreated');
+
+    if (!order) {
+        res.status(500).json({ success: false })
+    }
+
+    res.send(order)
+});
+
+router.post(`/`, async (req, res) => {
+
+
+    const orderItemsIds = Promise.all(req.body.orderItems.map(async item => {
+        let newOrderItem = new OrderItem({
+            quantity: item.quantity,
+            product: item.product
+        });
+
+        //await returns promise that's why need to be wrapped with Promise.all method on request body
+        newOrderItem = await newOrderItem.save();
+
+        return newOrderItem._id;
+    }));
+    console.log(orderItemsIds);//returns Promise { <pending> } (need to be resolved into id array)
+    const orderItemsIdsResolved = await orderItemsIds;
+    console.log(orderItemsIdsResolved);//returns [new ObjectId("6241d5df84a5cbd7b22659b7"),new ObjectId("6241d5df84a5cbd7b22659b8")]
+
+    let order = new Order({
+        orderItems: orderItemsIdsResolved,
+        shippingAddress1: req.body.shippingAddress1,
+        shippingAddress2: req.body.shippingAddress2,
+        city: req.body.city,
+        zip: req.body.zip,
+        country: req.body.country,
+        phone: req.body.phone,
+        status: req.body.status,
+        totalPrice: req.body.totalPrice,
+        user: req.body.user,
     })
 
-    order.save().then((createdOrder => {
-        res.status(201).json(createdOrder)
-    })).catch((err) => {
-        res.status(500).json({
-            error: err,
-            success: false
+    order = await order.save();
+
+    if (!order) {
+        return res.status(400).send({
+            message: "the order cannot be created"
         })
-    })
+    }
+
+    res.send(order);
+
+    // order.save().then((createdOrder => {
+    //     res.status(201).json(createdOrder)
+    // })).catch((err) => {
+    //     res.status(500).json({
+    //         error: err,
+    //         success: false
+    //     })
+    // })
 });
 
 module.exports = router;
